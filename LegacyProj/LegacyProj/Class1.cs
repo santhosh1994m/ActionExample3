83cdb8b5ccfe55c7c5afd00f28d51718a8f5999a
Terje Sandstrom <terje.sandstrom.personal@outlook.com>
2019-11-19 19:58:50 +0100

    Code added

---
+++ LegacyProj/LegacyProj/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyProj
+{
+    public class Math
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ LegacyProj/LegacyProjTests/MathTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using LegacyProj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyProj.Tests
+{
+    [TestFixture()]
+    public class MathTests
+    {
+        [Test()]
+        public void AddTest()
+        {
+            var sut = new Math();
+            var res = sut.Add(2, 3);
+            Assert.That(res, Is.EqualTo(5));
+        }
+    }
+}
